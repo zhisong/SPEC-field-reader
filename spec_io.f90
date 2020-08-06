96090740917f7c07bbce3e35b595f502d794f2db
spiffmeister <robospiff@gmail.com>
2020-08-06 22:30:03 +1000

    Updated SPEC-field-reader for new version of SPEC

--- spec_io.f90
+++ spec_io.f90
@@ -1,220 +1,220 @@
-! the module reads the SPEC output files
-module spec_io
+! Module for generating SPEC fields for LEVIS
+! 
+MODULE spec_io
 
-private
+PRIVATE
 
-  interface read_h5_data
-    module procedure read_h5_int, read_h5_int_array, read_h5_real_array_2d
-  end interface read_h5_data
+PUBLIC :: read_spec_h5
 
-public :: read_spec_h5, read_spec_field
+CONTAINS
+    
+    SUBROUTINE read_spec_h5(filename,sstate)
+        USE hdf5
+        USE spec_state ! The data structure for SPEC
 
-contains
-  subroutine read_spec_h5(filename, sstate)
-  ! read the SPEC hdf5 output
-  ! INPUTS:
-  ! filename - character(:), the hdf5 filename
-  ! OUTPUTS:
-  ! sstate
-    use hdf5
-    use spec_state
-    implicit none
-    character(LEN=*), intent(in) :: filename
-    type(state) :: sstate
+        IMPLICIT NONE
+        
+        CHARACTER(len=*), INTENT(IN) :: filename
+        TYPE(state) :: sstate
 
-    integer(HID_T) :: file_id,  dset_id
-    integer :: hdferr
+        INTEGER(HID_T) :: file_id, dset_id
+        INTEGER :: hdferr
+        INTEGER :: ivol, LradSize, Ll, Lu
+        INTEGER, DIMENSION(:), ALLOCATABLE :: Lrad
+        REAL, DIMENSION(:,:), ALLOCATABLE :: Ate, Ato, Aze, Azo
 
-    call h5open_f(hdferr)
-    call check(hdferr)
+        ! CHECK HDF5 is working
+        CALL h5open_f(hdferr)
+        CALL checkh5(hdferr)
 
-    call h5fopen_f(TRIM(filename), H5F_ACC_RDONLY_F, file_id, hdferr)
-    call check(hdferr)
+        ! Open the hdf5 file
+        CALL h5fopen_f(TRIM(filename), H5F_ACC_RDONLY_F, file_id, hdferr)
+        CALL checkh5(hdferr)
+        
+        !! Reading in base equilbrium data needed for reading/constructing all other data
+        ! Input data
+        CALL read_h5_int(file_id, 'input/physics/Igeometry', sstate%Igeometry)
+        CALL read_h5_int(file_id, 'input/physics/Istellsym', sstate%Istellsym)
+        CALL read_h5_int(file_id, 'input/physics/Lfreebound', sstate%Lfreebound)
+        CALL read_h5_int(file_id, 'input/physics/Nvol', sstate%Nvol)
+        CALL read_h5_int(file_id, 'input/physics/Nfp', sstate%Nfp)
+        CALL read_h5_int(file_id, 'input/physics/Mpol', sstate%Mpol)
+        CALL read_h5_int(file_id, 'input/physics/Ntor', sstate%Ntor)
+        ! Output data
+        CALL read_h5_int(file_id, 'output/Mvol', sstate%Mvol)
+        CALL read_h5_int(file_id, 'output/mn', sstate%mn)
+        
+        ALLOCATE(sstate%Lrad(sstate%Mvol))
+        CALL read_h5_int_array(file_id, 'input/physics/Lrad', sstate%Lrad, sstate%Mvol)
+        
+        !!!!!!!!!!!! VOLUME
+        ! Harmonics
+        sstate%Ri%Nfp = sstate%Nfp
+        sstate%Ri%Mvol = sstate%Mvol
+        sstate%Ri%icoordinatesingularity = (sstate%Igeometry .EQ. 2) .OR. (sstate%Igeometry .EQ. 3)
+        sstate%Ri%Igeometry = sstate%Igeometry
+        sstate%Ri%isym = (sstate%Istellsym .EQ. 1)
+        sstate%Ri%mn = sstate%mn
+        sstate%Ri%Mpol = sstate%Mpol
+        sstate%Ri%Ntor = sstate%Ntor
 
-    call read_h5_data(file_id, 'Igeometry', sstate%Igeometry)
-    call read_h5_data(file_id, 'Istellsym', sstate%Istellsym)
-    call read_h5_data(file_id, 'Lfreebound', sstate%Lfreebound)
-    call read_h5_data(file_id, 'Nvol', sstate%Nvol)
-    call read_h5_data(file_id, 'Mvol', sstate%Mvol)
-    call read_h5_data(file_id, 'Nfp', sstate%Nfp)
-    call read_h5_data(file_id, 'Mpol', sstate%Mpol)
-    call read_h5_data(file_id, 'Ntor', sstate%Ntor)
-    call read_h5_data(file_id, 'mn', sstate%mn)
-  
-    allocate(sstate%Lrad(sstate%Mvol))
-    call read_h5_data(file_id, 'Lrad', sstate%Lrad, sstate%Mvol)
+        ALLOCATE(sstate%Ri%im(sstate%mn))
+        ALLOCATE(sstate%Ri%in(sstate%mn))
+        CALL read_h5_int_array(file_id, 'output/im', sstate%Ri%im, sstate%mn)
+        CALL read_h5_int_array(file_id, 'output/in', sstate%Ri%in, sstate%mn)
 
-    sstate%Ri%nfp = sstate%Nfp
-    sstate%Ri%mvol = sstate%Mvol
-    sstate%Ri%icoordinatesingularity = sstate%Igeometry== 2 .or. sstate%Igeometry==3
-    sstate%Ri%igeometry = sstate%Igeometry
-    sstate%Ri%isym = sstate%Istellsym == 1
-    sstate%Ri%mn = sstate%mn
-    sstate%Ri%mpol = sstate%Mpol
-    sstate%Ri%ntor = sstate%Ntor
-    allocate(sstate%Ri%im(sstate%mn))
-    call read_h5_data(file_id, 'im', sstate%Ri%im, sstate%mn)
-    allocate(sstate%Ri%in(sstate%mn))
-    call read_h5_data(file_id, 'in', sstate%Ri%in, sstate%mn)
+        ! Amplitudes
+        ALLOCATE(sstate%Ri%Rbc(sstate%mn, 0:sstate%Mvol))
+        ALLOCATE(sstate%Ri%Rbs(sstate%mn, 0:sstate%Mvol))
+        ALLOCATE(sstate%Ri%Zbc(sstate%mn, 0:sstate%Mvol))
+        ALLOCATE(sstate%Ri%Zbs(sstate%mn, 0:sstate%Mvol))
+        CALL read_h5_real_array(file_id, 'output/Rbc', sstate%Ri%Rbc, (/sstate%mn, sstate%Mvol+1/))
+        CALL read_h5_real_array(file_id, 'output/Rbs', sstate%Ri%Rbs, (/sstate%mn, sstate%Mvol+1/))
+        CALL read_h5_real_array(file_id, 'output/Zbc', sstate%Ri%Zbc, (/sstate%mn, sstate%Mvol+1/))
+        CALL read_h5_real_array(file_id, 'output/Zbs', sstate%Ri%Zbs, (/sstate%mn, sstate%Mvol+1/))
+        
 
-    allocate(sstate%Ri%Rbc(sstate%mn, 0:sstate%Mvol))
-    allocate(sstate%Ri%Rbs(sstate%mn, 0:sstate%Mvol))
-    allocate(sstate%Ri%Zbc(sstate%mn, 0:sstate%Mvol))
-    allocate(sstate%Ri%Zbs(sstate%mn, 0:sstate%Mvol))
-    call read_h5_data(file_id, 'Rbc', sstate%Ri%Rbc, (/sstate%mn, sstate%Mvol+1/))
-    call read_h5_data(file_id, 'Rbs', sstate%Ri%Rbs, (/sstate%mn, sstate%Mvol+1/))
-    call read_h5_data(file_id, 'Zbc', sstate%Ri%Zbc, (/sstate%mn, sstate%Mvol+1/))
-    call read_h5_data(file_id, 'Zbs', sstate%Ri%Zbs, (/sstate%mn, sstate%Mvol+1/))
+        !!!!!!!!!!!! VECTOR POTENTIAL
+        ! Allocate space
+        LradSize = SIZE(sstate%Lrad)+SUM(sstate%Lrad)
+        ALLOCATE(sstate%A(sstate%Mvol))
 
-    call h5fclose_f(file_id, hdferr) 
+        ! Read in the vector potentials
+        ALLOCATE(Ate(LradSize,sstate%mn))
+        ALLOCATE(Aze(LradSize,sstate%mn))
+        ALLOCATE(Ato(LradSize,sstate%mn))
+        ALLOCATE(Azo(LradSize,sstate%mn))
+        CALL read_h5_real_array(file_id, 'vector_potential/Ate', Ate, (/LradSize,sstate%mn/))
+        CALL read_h5_real_array(file_id, 'vector_potential/Aze', Aze, (/LradSize,sstate%mn/))
+        CALL read_h5_real_array(file_id, 'vector_potential/Ato', Ato, (/LradSize,sstate%mn/))
+        CALL read_h5_real_array(file_id, 'vector_potential/Azo', Azo, (/LradSize,sstate%mn/))
 
-  end subroutine read_spec_h5
+        DO ivol = 1, sstate%Mvol
+            ! Index
+            Ll = SUM(sstate%Lrad(1:ivol-1))+SIZE(sstate%Lrad(1:ivol))
+            Lu = SUM(sstate%Lrad(1:ivol))+SIZE(sstate%Lrad(1:ivol))
+            ! Vector potential
+            ALLOCATE(sstate%A(ivol)%Ate(0:sstate%Lrad(ivol), sstate%mn))
+            ALLOCATE(sstate%A(ivol)%Aze(0:sstate%Lrad(ivol), sstate%mn))
+            ALLOCATE(sstate%A(ivol)%Ato(0:sstate%Lrad(ivol), sstate%mn))
+            ALLOCATE(sstate%A(ivol)%Azo(0:sstate%Lrad(ivol), sstate%mn))
+            sstate%A(ivol)%Ate(:,:) = Ate(Ll:Lu,1:sstate%mn)
+            sstate%A(ivol)%Aze(:,:) = Aze(Ll:Lu,1:sstate%mn)
+            sstate%A(ivol)%Ato(:,:) = Ato(Ll:Lu,1:sstate%mn)
+            sstate%A(ivol)%Azo(:,:) = Azo(Ll:Lu,1:sstate%mn)
+            ! Lrad
+            sstate%A(ivol)%Lrad = sstate%Lrad(ivol)
 
-  subroutine read_spec_field(filename, sstate)
-  ! read the SPEC field output
-  ! INPUTS:
-  ! filename - character(:), the .A filename
-  ! OUTPUTS:
-  ! sstate
-    use spec_state
-    implicit none
-    character(LEN=*), intent(in) :: filename
-    type(state) :: sstate
+            ! im and in
+            ALLOCATE(sstate%A(ivol)%im(sstate%mn))
+            ALLOCATE(sstate%A(ivol)%in(sstate%mn))
+            sstate%A(ivol)%im = (sstate%Ri%im)
+            sstate%A(ivol)%in = (sstate%Ri%in)
+            
+            sstate%A(ivol)%Isingular = .FALSE. !Correct for this after loop
+            sstate%A(ivol)%Isym = (sstate%Istellsym .EQ. 1)
+            sstate%A(ivol)%Nfp = sstate%Nfp
+            sstate%A(ivol)%mn = sstate%mn
 
-    integer :: aunit
-    integer :: Mvol, Mpol, Ntor, mn, Nfp, Lrad
-    integer :: vvol, ii
-    integer, dimension(:), allocatable :: im, in
-    !!! be careful here, one should really care about SMALL/BIG ENDIAN here
-    open(aunit, file=filename, action="read", form="unformatted" )
+        END DO
+        ! Coodinate singularity
+        sstate%A(1)%isingular = (sstate%Ri%icoordinatesingularity .EQV. .TRUE.)
+        
+        ! Deallocate temp variables
+        DEALLOCATE(Ate)
+        DEALLOCATE(Aze)
+        DEALLOCATE(Ato)
+        DEALLOCATE(Azo)
 
-    read(aunit) Mvol, Mpol, Ntor, mn, Nfp
+    END SUBROUTINE
 
-    if (sstate%Mvol .ne. Mvol) stop "please check consistency between .h5 and .A files"
-    if (sstate%Mpol .ne. Mpol) stop "please check consistency between .h5 and .A files"
-    if (sstate%Ntor .ne. Ntor) stop "please check consistency between .h5 and .A files"
-    if (sstate%Nfp .ne. Nfp) stop "please check consistency between .h5 and .A files"
+    !!!! READING SUBROUTINES !!!!
+    SUBROUTINE read_h5_int(file_id, name, data_out)
+        ! USED FOR READING IN INTEGER VALUES
+        USE HDF5
+        IMPLICIT NONE
+        INTEGER(HID_T), INTENT(IN) :: file_id
+        CHARACTER(LEN=*), INTENT(IN) :: name
+        INTEGER, DIMENSION(1) :: tmp_data
+        INTEGER, INTENT(OUT) ::  data_out
+    
+        INTEGER(HID_T) :: dset_id
+        INTEGER :: hdferr
+        INTEGER(HSIZE_T), DIMENSION(1) :: len = (/1/)
 
-    allocate(im(mn))
-    allocate(in(mn))
-    allocate(sstate%A(Mvol))
+        ! Open dataset
+        CALL h5dopen_f(file_id, name, dset_id, hdferr)
+        CALL checkh5(hdferr)
+        ! Read dataset
+        CALL h5dread_f(dset_id, H5T_NATIVE_INTEGER, tmp_data, len, hdferr)
+        CALL checkh5(hdferr)
+        ! Close dataset
+        CALL h5dclose_f(dset_id,hdferr)
+        CALL checkh5(hdferr)
+        ! Output data
+        data_out = tmp_data(1)
 
-    read(aunit) im(1:mn)
-    read(aunit) in(1:mn)
+    END SUBROUTINE read_h5_int
 
-    do vvol = 1, Mvol
-      read(aunit) Lrad
-      if (sstate%Lrad(vvol) .ne. Lrad) stop "please check consistency between .h5 and .A files"
-      sstate%A(vvol)%lrad = Lrad
+    SUBROUTINE read_h5_int_array(file_id, name, data_out, nsize)
+        use hdf5
+        IMPLICIT NONE
+        INTEGER(HID_T), INTENT(IN) :: file_id
+        CHARACTER(LEN=*), INTENT(IN) :: name
+        INTEGER, DIMENSION(nsize), INTENT(OUT) ::  data_out
+        INTEGER, INTENT(IN) :: nsize
+    
+        INTEGER(HID_T) :: dset_id
+        INTEGER :: hdferr
+        INTEGER(HSIZE_T), DIMENSION(1) :: len
+    
+        len = (/nsize/)
+        ! Open dataset
+        CALL h5dopen_f(file_id, name, dset_id, hdferr)
+        CALL checkh5(hdferr)
+        ! Read dataset
+        CALL h5dread_f(dset_id, H5T_NATIVE_INTEGER, data_out, len, hdferr)
+        CALL checkh5(hdferr)
+        ! Close dataset
+        CALL h5dclose_f(dset_id,hdferr)
+        CALL checkh5(hdferr)
 
-      sstate%A(vvol)%mn = mn
-      sstate%A(vvol)%Nfp = Nfp
-      allocate(sstate%A(vvol)%im(mn))
-      allocate(sstate%A(vvol)%in(mn))
-      sstate%A(vvol)%im(:) = im(:)
-      sstate%A(vvol)%in(:) = in(:)
+    END SUBROUTINE read_h5_int_array
+    
+    SUBROUTINE read_h5_real_array(file_id, name, data_out, nsize)
+        USE hdf5
+        IMPLICIT NONE
+        INTEGER(HID_T), INTENT(IN) :: file_id
+        CHARACTER(LEN=*), INTENT(IN) :: name
+        REAL, DIMENSION(:,:), INTENT(OUT) ::  data_out
+        INTEGER, DIMENSION(2),INTENT(IN) :: nsize
+    
+        INTEGER(HID_T) :: dset_id
+        INTEGER :: hdferr
+        INTEGER(HSIZE_T), DIMENSION(2) :: len
+        
+        len(:) = nsize(:)
+        ! Open dataset
+        CALL h5dopen_f(file_id, name, dset_id, hdferr)
+        CALL checkh5(hdferr)
+        ! Read dataset
+        CALL h5dread_f(dset_id, H5T_NATIVE_DOUBLE, data_out, len, hdferr)
+        CALL checkh5(hdferr)
+        ! Close dataset
+        CALL h5dclose_f(dset_id,hdferr)
+        CALL checkh5(hdferr)
 
-      if (sstate%Istellsym == 1) then
-        sstate%A(vvol)%isym = .true.
-      else
-        sstate%A(vvol)%isym = .false.
-      end if
+    END SUBROUTINE read_h5_real_array
 
-      if (sstate%Ri%icoordinatesingularity .and. vvol.eq.1) then
-        sstate%A(vvol)%isingular = .true.
-      else
-        sstate%A(vvol)%isingular = .false.
-      end if
+    SUBROUTINE checkh5(ierr)
+        ! Check to make sure no errors occured with hdf5 interface
+        IMPLICIT NONE
+        INTEGER, INTENT(IN) :: ierr
+        IF (ierr.NE.0) THEN
+            STOP "hdf5 error"
+        END IF
+    END SUBROUTINE
 
-      allocate(sstate%A(vvol)%Ate(0:Lrad, mn))
-      allocate(sstate%A(vvol)%Aze(0:Lrad, mn))
-      allocate(sstate%A(vvol)%Ato(0:Lrad, mn))
-      allocate(sstate%A(vvol)%Azo(0:Lrad, mn))
+END MODULE
 
-      do ii = 1, mn ! loop over Fourier harmonics;
 
-        read(aunit) sstate%A(vvol)%Ate(0:Lrad, ii)
-        read(aunit) sstate%A(vvol)%Aze(0:Lrad, ii)
-        read(aunit) sstate%A(vvol)%Ato(0:Lrad, ii)
-        read(aunit) sstate%A(vvol)%Azo(0:Lrad, ii)
-      
-      enddo
-
-    end do
-
-    deallocate(im)
-    deallocate(in)
-
-    close(aunit)
-  end subroutine read_spec_field
-
-!******* internal subroutines **********
-
-  subroutine read_h5_int(file_id, name, data)
-    use hdf5
-    implicit none
-    integer(HID_T), intent(in) :: file_id
-    character(LEN=*), intent(in) :: name
-    integer, intent(out) ::  data
-
-    integer(HID_T) :: dset_id
-    integer :: hdferr
-    integer, dimension(1) :: data_out
-    integer(HSIZE_T), dimension(1) :: len
-
-    len = (/1/)
-    call h5dopen_f(file_id, name, dset_id, hdferr)
-    call check(hdferr)
-    call h5dread_f(dset_id, H5T_NATIVE_INTEGER, data_out, len, hdferr)
-    call check(hdferr)
-    data = data_out(1)
-  end subroutine read_h5_int
-
-  subroutine read_h5_int_array(file_id, name, data, nsize)
-    use hdf5
-    implicit none
-    integer(HID_T), intent(in) :: file_id
-    character(LEN=*), intent(in) :: name
-    integer, dimension(nsize), intent(out) ::  data
-    integer, intent(in) :: nsize
-
-    integer(HID_T) :: dset_id
-    integer :: hdferr
-    integer, dimension(nsize) :: data_out
-    integer(HSIZE_T), dimension(1) :: len
-
-    if (nsize .ne. SIZE(data)) stop 'internal consistency when reading data'
-    len = (/nsize/)
-    call h5dopen_f(file_id, name, dset_id, hdferr)
-    call check(hdferr)
-    call h5dread_f(dset_id, H5T_NATIVE_INTEGER, data_out, len, hdferr)
-    call check(hdferr)
-    data(:) = data_out(:)
-  end subroutine read_h5_int_array
-
-subroutine read_h5_real_array_2d(file_id, name, data, nsize)
-    use hdf5
-    implicit none
-    integer(HID_T), intent(in) :: file_id
-    character(LEN=*), intent(in) :: name
-    real, dimension(:,:), intent(out) ::  data
-    integer, dimension(2),intent(in) :: nsize
-
-    integer(HID_T) :: dset_id
-    integer :: hdferr
-    integer(HSIZE_T), dimension(2) :: len
-
-    if (PRODUCT(nsize) .ne. SIZE(data)) stop 'internal consistency when reading data'
-    len(:) = nsize(:)
-    call h5dopen_f(file_id, name, dset_id, hdferr)
-    call check(hdferr)
-    call h5dread_f(dset_id, H5T_NATIVE_DOUBLE, data, len, hdferr)
-    call check(hdferr)
-  end subroutine read_h5_real_array_2d
-
-  subroutine check(ierr)
-    implicit none
-    integer, intent(in) :: ierr
-    if (ierr.ne.0) stop "hdf5 error!"
-  end subroutine
-end module spec_io
